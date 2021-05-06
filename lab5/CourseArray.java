86c5bd9aff53e5e7831dd6facd7d37fd507a90e5
arvestad <arve@math.su.se>
2021-05-06 15:36:14 +0200

    Added test to ensure that the sorting is correct.

--- lab5/CourseArray.java
+++ lab5/CourseArray.java
@@ -37,7 +37,7 @@ public class CourseArray {
      * Input: none
      * Output: none
      * Side effect: sorts the 'courses' attribute
-     * Time complexity: O(n lg n), where n is the number of elements in the course array.
+     * Time complexity: O(n lg n) comparisons, where n is the number of elements in the course array.
      */
     public void mergeSort() {
         // To implement
@@ -66,6 +66,15 @@ public class CourseArray {
         }
     }
 
+    public boolean sorted() {
+        for (int i=1; i<courses.size(); i++) {
+            if (courses.get(i).compareTo(courses.get(i-1)) < 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     public static void main(String[] args) {
         // We create 3 CourseArray objects. They contain the same
@@ -85,6 +94,17 @@ public class CourseArray {
         checkpoints[2] = System.currentTimeMillis();
         courses3.javaSort();
         checkpoints[3] = System.currentTimeMillis();
+
+        // Ensure correct sorting
+        if (!courses1.sorted()) {
+            System.out.println("courses1 is not sorted");
+        }
+        if (!courses2.sorted()) {
+            System.out.println("courses2 is not sorted");
+        }
+        if (!courses3.sorted()) {
+            System.out.println("courses3 is not sorted");
+        }
 
         String algs[] = {"", "selectionSort", "mergeSort", "javaSort"};
         // Output timing results
